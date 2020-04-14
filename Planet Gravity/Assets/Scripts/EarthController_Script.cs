a867652a5812b31ddf85a1f45608aed15484394c
Wayra Codes <WayraCodes@github.com>
2020-04-14 10:44:27 -0400

    Initial Commit

---
+++ Planet Gravity/Assets/Scripts/EarthController_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthController_Script : MonoBehaviour
+{
+    private Transform Player;
+    private bool PlayerInSight = false;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Formula: F = G m1 m2 / r^2
+
+    private void FixedUpdate()
+    {
+        if (PlayerInSight)
+        {
+            GravitationalPull();
+        }
+    }
+
+    void GravitationalPull()
+    {
+        Vector3 Diference = transform.position - Player.transform.position;
+        float Distance = Diference.magnitude;
+        Vector3 GravityDireccion = Diference.normalized;
+        float Gravity = 9.8f * (this.transform.localScale.x * Player.transform.localScale.x * 20) / (Distance * Distance);
+        Vector3 GravityVector = (GravityDireccion * Gravity);
+        Player.transform.GetComponent<Rigidbody2D>().AddForce(GravityVector, ForceMode2D.Force);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInSight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInSight = false;
+        }
+    }
+}
---
+++ Planet Gravity/Assets/Scripts/PlayerController_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Script : MonoBehaviour
+{
+    public Joystick MovementJoystick;
+    private float Speed = 10f;
+    private Rigidbody2D rb;
+    private Vector3 Direction;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Direction = Vector3.up * -MovementJoystick.Vertical + Vector3.right * -MovementJoystick.Horizontal;
+        rb.AddForce(Direction * Speed, ForceMode2D.Force);
+    }
+}
